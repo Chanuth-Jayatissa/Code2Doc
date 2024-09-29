5e8fc7add2a7d2a77215f2bc23955f50a4e8b756
Karthikeya Thota <karthikeya@clay.local>
2024-09-29 04:01:53 -0400

    Added Signup Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
-import Services from "./components/Services";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Signup from "./components/Signup";
 
 const App = () => {
   return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Footer />
-      </div>
-
-      <ButtonGradient />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+        <Route path="/login" element={<Signup/>} />
+      </Routes>
+    </div>
   );
 };
 
