6748eb3f40e03943abd90601c6813844a5946ca5
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2024-09-28 22:08:08 -0400

    added all recent files

---
+++ my-app/app/ui/sidenav.tsx
@@ -0,0 +1,13 @@
+export default function SideNav() {
+    return(
+        <div className="bg-gray-100 w-1/4 h-full p-4">
+            <h2 className="text-xl font-bold mb-4">Class dog</h2>
+            <ul className="space-y-2">
+                <li>Shit</li>
+                <li>Eat</li>
+                <li>Poop</li>
+            </ul>
+            <h2 className="text-xl font-bold mt-6">Class cat</h2>
+        </div>
+    )
+}
